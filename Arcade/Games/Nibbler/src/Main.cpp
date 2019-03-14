005660d87c3a1032216abac2d0d095c291c14771
Victor Neuret <victor.neuret@epitech.eu>
2019-03-14 14:44:24 +0100

    Merge development to master (#2)
    * Initial commit: (#1)
    
    * Initial commit:
    
    - Base Makefile and Targets Makefiles
    - Empty mains for each targets
    - .gitignore
    
    * Remove useless line in .gitignore
    
    * Rename NDK++ to ncurses

---
+++ Arcade/Games/Nibbler/src/Main.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** arcade
+** File description:
+** Main.cpp
+*/
+
+int main()
+{
+    return 0;
+}
---
+++ games/README.md
@@ -0,0 +1,3 @@
+# OOP_arcade_2018
+
+Put games libraries in this directory.
