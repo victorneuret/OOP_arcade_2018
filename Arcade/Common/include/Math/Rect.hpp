b426b851349487d5bf8a8122d7d4dfeb0ad32842
victorneuret <victor.neuret@epitech.eu>
2019-03-18 14:44:33 +0100

    Remove getter and setter for Rect struct

--- Arcade/Common/include/Math/Rect.hpp
+++ Arcade/Common/include/Math/Rect.hpp
@@ -16,11 +16,4 @@ struct Rect {
     Rect(const Vector &pos, const Vector &size) noexcept;
     Rect(double x, double y, double w, double h) noexcept;
     ~Rect() = default;
-
-    Vector getPosition() const noexcept;
-    void setPosition(const Vector &newPos) noexcept;
-    void setPosition(const double x, const double y) noexcept;
-    Vector getSize() const noexcept;
-    void setSize(const Vector &newSize) noexcept;
-    void setSize(const double w, const double h) noexcept;
 };
--- Arcade/Common/src/Rect.cpp
+++ Arcade/Common/src/Rect.cpp
@@ -14,35 +14,3 @@ Rect::Rect(const Vector &pos, const Vector &size) noexcept
 Rect::Rect(double x, double y, double w, double h) noexcept
     : pos(Vector(x, y)), size(Vector(w, h))
 {}
-
-Vector Rect::getPosition() const noexcept
-{
-    return pos;
-}
-
-void Rect::setPosition(const Vector &newPos) noexcept
-{
-    pos = newPos;
-}
-
-void Rect::setPosition(const double x, const double y) noexcept
-{
-    pos.x = x;
-    pos.y = y;
-}
-
-Vector Rect::getSize() const noexcept
-{
-    return size;
-}
-
-void Rect::setSize(const Vector &newSize) noexcept
-{
-    size = newSize;
-}
-
-void Rect::setSize(const double w, const double h) noexcept
-{
-    size.x = w;
-    size.y = h;
-}
