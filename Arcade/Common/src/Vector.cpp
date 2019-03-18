bbefa216b5237b32e28153accf6a8f3708832041
victorneuret <victor.neuret@epitech.eu>
2019-03-18 01:44:59 +0100

    Add Vector

---
+++ Arcade/Common/src/Vector.cpp
@@ -0,0 +1,135 @@
+/*
+** EPITECH PROJECT, 2019
+** arcade
+** File description:
+** Vector.cpp
+*/
+
+#include <cstdint>
+
+#include "include/Math/Vector.hpp"
+
+
+Vector::Vector(double x, double y) noexcept
+        : x(x), y(y)
+{}
+
+Vector &Vector::operator+=(const Vector &vec) noexcept
+{
+    x += vec.x;
+    y += vec.y;
+    return *this;
+}
+
+Vector &Vector::operator+=(const double &val) noexcept
+{
+    x += val;
+    y += val;
+    return *this;
+}
+
+Vector &Vector::operator-=(const Vector &vec) noexcept
+{
+    x -= vec.x;
+    y -= vec.y;
+    return *this;
+}
+
+Vector &Vector::operator-=(const double &val) noexcept
+{
+    x -= val;
+    y -= val;
+    return *this;
+}
+
+Vector &Vector::operator*=(const Vector &vec) noexcept
+{
+    x *= vec.x;
+    y *= vec.y;
+    return *this;
+}
+
+Vector &Vector::operator*=(const double &val) noexcept
+{
+    x *= val;
+    y *= val;
+    return *this;
+}
+
+Vector &Vector::operator/=(const Vector &vec) noexcept
+{
+    x /= vec.x;
+    y /= vec.y;
+    return *this;
+}
+
+Vector &Vector::operator/=(const double &val) noexcept
+{
+    x /= val;
+    y /= val;
+    return *this;
+}
+
+Vector Vector::operator+(const Vector &vec) const noexcept
+{
+    return Vector(x + vec.x, y + vec.y);
+}
+
+Vector Vector::operator+(const double &val) const noexcept
+{
+    return Vector(x + val, y + val);
+}
+
+Vector Vector::operator-(const Vector &vec) const noexcept
+{
+    return Vector(x - vec.x, y - vec.y);
+}
+
+Vector Vector::operator-(const double &val) const noexcept
+{
+    return Vector(x - val, y - val);
+}
+
+Vector Vector::operator*(const Vector &vec) const noexcept
+{
+    return Vector(x * vec.x, y * vec.y);
+}
+
+Vector Vector::operator*(const double &val) const noexcept
+{
+    return Vector(x * val, y * val);
+}
+
+Vector Vector::operator/(const Vector &vec) const noexcept
+{
+    return Vector(x / vec.x, y / vec.y);
+}
+
+Vector Vector::operator/(const double &val) const noexcept
+{
+    return Vector(x / val, y / val);
+}
+
+Vector &Vector::operator=(const Vector &vec) noexcept
+{
+    x = vec.x;
+    y = vec.y;
+    return *this;
+}
+
+Vector &Vector::operator=(const double &val) noexcept
+{
+    x = val;
+    y = val;
+    return *this;
+}
+
+bool Vector::operator==(const Vector &vec) const noexcept
+{
+    return (x == vec.x && y == vec.y);
+}
+
+bool Vector::operator!=(const Vector &vec) const noexcept
+{
+    return (x != vec.x || y != vec.y);
+}
