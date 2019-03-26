8d3f0f3afe343dab18fcc87ad0fe65cfc7b2a9cc
victorneuret <victor.neuret@epitech.eu>
2019-03-27 00:06:03 +0100

    Change SDL window size and SDL keybinds

---
+++ Arcade/Games/Nibbler/include/Nibbler.hpp
@@ -0,0 +1,8 @@
+//
+// Created by victor on 3/26/19.
+//
+
+#ifndef ARCADE_NIBBLER_HPP
+#define ARCADE_NIBBLER_HPP
+
+#endif //ARCADE_NIBBLER_HPP
---
+++ Arcade/Games/Nibbler/src/Nibbler.cpp
@@ -0,0 +1,4 @@
+//
+// Created by victor on 3/26/19.
+//
+
